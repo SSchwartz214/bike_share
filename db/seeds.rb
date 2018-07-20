869c6415a8cc278c6ebae158614f2a085d07685c
Connor <connorosburn@gmail.com>
2018-07-20 17:26:16 -0600

    Splitting up work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_data(path, data_type)
+  raw_input = CSV.open(path, headers: true, header_converters: :symbol)
+  raw_input.each do |pre_hash|
+    data = pre_hash.to_h
+    data_type.create(data)
+  end
+end

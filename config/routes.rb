287cc0173b187e293035c01e31066bca3c5f26a0
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-21 16:38:41 -0600

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :stations, param: :slug, only: [:index]
+  resources :trips, only: [:index, :show]
 
   get '/:slug', to: 'stations#show'
 end

5e09eb23ac14b24355fe7656b58c890ac94ef15f
Connor <connorosburn@gmail.com>
2018-07-21 16:21:14 -0600

    Got conditions to work

--- spec/features/conditions/visitor_conditions_spec.rb
+++ spec/features/conditions/visitor_conditions_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "a visitor" do
   describe "conditions index" do
     it "can see all conditions and attributes" do
-      condition_1 = Condition.create!(end_date: "8/29/2013", max_temperature: 1234, mean_temperuate: 511, min_temperature: 123, mean_humididity: 75, mean_visibility: 10, mean_wind_speed: 27, precipitation: 11)
+      condition_1 = Condition.create!(end_date: "8/29/2013", max_temperature: 1234, mean_temperature: 511, min_temperature: 123, mean_humididity: 75, mean_visibility: 10, mean_wind_speed: 27, precipitation: 11)
 
-      condition_1 = Condition.create!(end_date: "8/29/2017", max_temperature: 12345, mean_temperuate: 5115, min_temperature: 1235, mean_humididity: 755, mean_visibility: 105, mean_wind_speed: 275, precipitation: 115)
+      condition_2 = Condition.create!(end_date: "8/29/2017", max_temperature: 12345, mean_temperature: 5115, min_temperature: 1235, mean_humididity: 755, mean_visibility: 105, mean_wind_speed: 275, precipitation: 115)
 
-      visit conditions_index_path
+      visit conditions_path
 
       expect(page).to have_content(condition_1.end_date)
       expect(page).to have_content(condition_1.max_temperature)
@@ -31,7 +31,7 @@ describe "a visitor" do
 
   describe "conditions show" do
     it "can see all attributes for condition" do
-      condition_1 = Condition.create!(end_date: "8/29/2013", max_temperature: 1234, mean_temperuate: 511, min_temperature: 123, mean_humididity: 75, mean_visibility: 10, mean_wind_speed: 27, precipitation: 11)
+      condition_1 = Condition.create!(end_date: "8/29/2013", max_temperature: 1234, mean_temperature: 511, min_temperature: 123, mean_humididity: 75, mean_visibility: 10, mean_wind_speed: 27, precipitation: 11)
 
       visit condition_path(condition_1)
 

1397fe17768a5bd174cce8bc0956e69009b219c9
Connor <connorosburn@gmail.com>
2018-07-22 15:30:23 -0600

    Deleted some broken code

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,18 +3,9 @@ class Station < ApplicationRecord
   validates_presence_of :dock_count
   validates_presence_of :city
   validates_presence_of :installation_date
-  after_create :store_date_attributes
 
   def to_param
     slug
-  end
-
-  def store_date_attributes
-    date_hash = parse_date_string(installation_date)
-
-    @installation_day = date_hash[:day]
-    @installation_month = date_hash[:month]
-    @installation_year = date_hash[:year]
   end
 
   def self.average_bikes
@@ -38,8 +29,5 @@ class Station < ApplicationRecord
   end
 
   def self.newest
-    newest_by_year = find_by(installation_year: maximum(:installation_year))
-    newest_by_month = newest_by_year.find_by(installation_month: newest_by_year.maximum(:installation_month))
-    newest_by_month.find_by(installation_day: newest_by_month.maximum(:installation_day))
   end
 end

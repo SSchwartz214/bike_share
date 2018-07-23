6c736576292efab411ff78987fdee3c422860f37
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-22 18:37:42 -0600

    fix tests broken by schema and seed change

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -73,7 +73,7 @@ describe Trip, type: :model do
       station_6 = Station.create!(name: "fvfdfr", dock_count: 20, city: "09190ajsd0j", installation_date: "8/12/2013")
 
       trip_1 = Trip.create!(duration: 100, start_date: "8/29/2013 14:13", start_station: station_1, end_date: "8/29/2013 14:14", end_station: station_2, subscription_type: "subscribed", zip_code: 12345, bike_id: 1)
-      trip_2 = Trip.create!(duration: 200, start_date: "8/28/2013 14:19", start_station: station_3, end_date: "8/29/2013 14:17", end_station: station_4, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
+      trip_2 = Trip.create!(duration: 200, start_date: "8/28/2013 14:19", start_station: station_3, date: "8/29/2013 14:17", end_station: station_4, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
       trip_3 = Trip.create!(duration: 300, start_date: "8/28/2013 14:19", start_station: station_1, end_date: "8/29/2013 14:17", end_station: station_6, subscription_type: "visitor", zip_code: 12446, bike_id: 3)
 
       expect(Trip.most_starting_rides).to eq(station_1)

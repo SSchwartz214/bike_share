40c3dbc38ee8a45c8a5bab80ff471d16a9cb2e0d
Seth Schwartz <sschwartz214@gmail.com>
2018-07-23 23:48:45 -0600

    makes test for edit

---
+++ spec/features/trips/admin_edits_a_trip_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+#   As an admin user,
+# When I visit admin trip edit,
+# I fill in a form with all trip attributes,
+# When I click "Update Trip",
+# I am directed to that trip's show page,
+# I see the updated trip's information,
+# I also see a flash message that I have updated that trip.
+#
+# ** All Attributes must be present **
+describe 'user visits trip edit page' do
+  context 'as an admin' do
+    it 'allows admin to edit a trip' do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+      station_1 = Station.create!(name: "aiojd", dock_count: 8, city: "0912jeioj", installation_date: "8/6/2013")
+      station_2 = Station.create!(name: "ajsd0jd", dock_count: 20, city: "09190ajsd0j", installation_date: "8/12/2013")
+      trip = Trip.create!(duration: 123, start_date: DateTime.strptime("8/29/2013 14:13", '%m/%d/%Y %H:%M'), start_station: station_1, end_date: DateTime.strptime("8/29/2013 14:14", '%m/%d/%Y %H:%M'), end_station: station_2, subscription_type: "subscribed", zip_code: 12345, bike_id: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit edit_admin_trip_path(trip)
+
+      fill_in 'trip_duration', with: 100
+      fill_in 'trip_start_date', with: "8/30/2013"
+      fill_in 'trip_start_station', with: station_2
+      fill_in 'trip_end_date', with: "9/01/2013"
+      fill_in 'trip_end_station', with: station_1
+      fill_in 'trip_subscription_type', with: "subscribed"
+      fill_in 'trip_zip_code', with: 12345
+      fill_in 'trip_bike_id', with: 2
+
+      click_button "Update Trip"
+
+      expect(current_path).to eq(admin_trip_path(trip))
+    end
+  end
+end

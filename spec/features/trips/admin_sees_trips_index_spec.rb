a5061c8334ea6c6bf582dd6e0daef40ec02d1efe
Seth Schwartz <sschwartz214@gmail.com>
2018-07-24 17:52:54 -0600

    adds index action, view and test for admin trips

--- spec/features/trips/admin_sees_trips_index_spec.rb
+++ spec/features/trips/admin_sees_trips_index_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'user visits trip edit page' do
-  context 'as an admin' do
-    it 'allows admin to edit a trip' do
+describe "User visits trips index page" do
+  context "as admin" do
+    it "allows admin to see all trips" do
       admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
       station_1 = Station.create!(name: "aiojd", dock_count: 8, city: "0912jeioj", installation_date: "8/6/2013")
       station_2 = Station.create!(name: "ajsd0jd", dock_count: 20, city: "09190ajsd0j", installation_date: "8/12/2013")
@@ -10,20 +10,8 @@ describe 'user visits trip edit page' do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit edit_admin_trip_path(trip)
-
-      fill_in :trip_duration, with: 100
-      fill_in :trip_start_date, with: "8/30/2013"
-      find('#trip_start_station_id').find(:xpath, "option[2]").select_option
-      fill_in :trip_end_date, with: "9/01/2013"
-      find('#trip_end_station_id').find(:xpath, "option[1]").select_option
-      fill_in :trip_subscription_type, with: "subscribed"
-      fill_in :trip_zip_code, with: 12345
-      fill_in :trip_bike_id, with: 2
-
-      click_button "Update Trip"
-
-      expect(current_path).to eq(admin_trip_path(trip))
+      visit admin_trips_path
+      expect(page).to have_content("Admin Trips")
     end
   end
 end

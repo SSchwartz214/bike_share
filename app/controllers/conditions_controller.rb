29bd8018d08c494c0332df2383add54de391dd16
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-23 19:56:55 -0600

    add edit functionality to admin user for conditions

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,4 +1,6 @@
 class ConditionsController < ApplicationController
+  before_action :set_condition, only: [:show, :edit, :update, :destroy]
+
   def index
     @conditions = Condition.paginate(:page => params[:page], :per_page => 50)
   end
@@ -7,4 +9,28 @@ class ConditionsController < ApplicationController
     @condition = Condition.find(params[:id])
   end
 
+  def edit
+
+  end
+
+  def update
+    @condition.update(condition_params)
+    if @condition.save
+      flash[:success] = "Weather updated!"
+      redirect_to condition_path(@condition)
+    else
+      flash.now[:alert] = @condition.errors.full_messages.join("<br>").html_safe
+      render :edit
+    end
+  end
+
+  private
+
+  def set_condition
+    @condition = Condition.find(params[:id])
+  end
+
+  def condition_params
+    params.require(:condition).permit(:date, :mean_temperature_f, :max_temperature_f, :min_temperature_f, :mean_humidity, :mean_visibility_miles, :mean_wind_speed_mph, :precipitation_inches)
+  end
 end
--- spec/features/conditions/admin_condition_spec.rb
+++ spec/features/conditions/admin_condition_spec.rb
@@ -19,5 +19,60 @@ describe "an admin" do
 
       expect(current_path).to eq(edit_condition_path(condition_1))
     end
+
+    it "can not see delete or edit buttons if it is not an admin" do
+      condition_1 = Condition.create!(date: Date.strptime("8/29/2013", '%m/%d/%Y'), max_temperature_f: 1234, mean_temperature_f: 511, min_temperature_f: 123, mean_humidity: 75, mean_visibility_miles: 10, mean_wind_speed_mph: 27, precipitation_inches: 11)
+
+      default = User.create!(first_name: "keegan", last_name: "c", username: "oijads", password: "oiajsiod")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default)
+
+      visit condition_path(condition_1)
+
+      expect(page).to_not have_content("Edit")
+      expect(page).to_not have_content("Delete")
+    end
+
+    it "fills out form to change condition" do
+      condition_1 = Condition.create!(date: Date.strptime("8/29/2013", '%m/%d/%Y'), max_temperature_f: 1234, mean_temperature_f: 511, min_temperature_f: 123, mean_humidity: 75, mean_visibility_miles: 10, mean_wind_speed_mph: 27, precipitation_inches: 11)
+
+      admin = User.create!(first_name: "keegan", last_name: "c", username: "oijads", password: "oiajsiod", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      date = "08/09/2013"
+      max_temp = 100
+      mean_temp = 50
+      min_temp = 1
+      mean_hum = 10
+      mean_vis = 20
+      wind_spd = 80
+      prec = 25
+
+      visit edit_condition_path(condition_1)
+
+      fill_in "condition[date]", with: date
+      fill_in "condition[mean_temperature_f]", with: mean_temp
+      fill_in "condition[max_temperature_f]", with: max_temp
+      fill_in "condition[min_temperature_f]", with: min_temp
+      fill_in "condition[mean_humidity]", with: mean_hum
+      fill_in "condition[mean_visibility_miles]", with: mean_vis
+      fill_in "condition[mean_wind_speed_mph]", with: wind_spd
+      fill_in "condition[precipitation_inches]", with: prec
+
+      click_on "Control the weather"
+
+      expect(current_path).to eq(condition_path(condition_1))
+
+      expect(page).to have_content("Weather updated!")
+
+      expect(page).to have_content(max_temp)
+      expect(page).to have_content(min_temp)
+      expect(page).to have_content(mean_temp)
+      expect(page).to have_content(mean_hum)
+      expect(page).to have_content(mean_vis)
+      expect(page).to have_content(wind_spd)
+      expect(page).to have_content(prec)
+    end
   end
 end

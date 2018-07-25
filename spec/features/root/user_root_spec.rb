6feb105826aa1f18b799f8d06c8459d3515476fc
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-24 20:40:19 -0600

    add tests for edit user, add edit button to dashboard

--- spec/features/root/user_root_spec.rb
+++ spec/features/root/user_root_spec.rb
@@ -24,5 +24,40 @@ describe "a registered user" do
       expect(page).to have_content("Logout")
       expect(page).to_not have_content("Login")
     end
+
+    it "can edit it's information" do
+      user_1 = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      new_first_name = "oi90js"
+      new_last_name = "o12e890dasj"
+
+      visit dashboard_path
+
+      expect(page).to have_content(user_1.first_name)
+      expect(page).to have_content(user_1.last_name)
+
+      click_on "Edit"
+
+      expect(current_path).to eq(edit_user_path(user_1))
+
+      fill_in :user_first_name, with: new_first_name
+      fill_in :user_first_name, with: new_last_name
+
+      click_on "Update"
+
+      expect(current_path).to eq(dashboard_path)
+
+      expect(page).to have_content(new_first_name)
+      expect(page).to have_content(new_last_name)
+    end
+
+    xit "can not update it's information if it is not logged in" do
+
+      visit dashboard_path
+
+      expect(page).to_not have_content("Edit")
+    end
   end
 end

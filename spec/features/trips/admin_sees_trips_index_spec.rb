1624e757010049cb2854b3a7fe90753072666190
Seth Schwartz <sschwartz214@gmail.com>
2018-07-24 18:14:21 -0600

    Adds links to trips index page for delete and edit.  Deletes admin index page and routes to default index page

--- spec/features/trips/admin_sees_trips_index_spec.rb
+++ spec/features/trips/admin_sees_trips_index_spec.rb
@@ -10,8 +10,22 @@ describe "User visits trips index page" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit admin_trips_path
-      expect(page).to have_content("Admin Trips")
+      visit trips_path
+      expect(page).to have_content("Trips")
     end
+  end
+  it 'allows admin to edit a trip' do
+    admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+    station_1 = Station.create!(name: "aiojd", dock_count: 8, city: "0912jeioj", installation_date: "8/6/2013")
+    station_2 = Station.create!(name: "ajsd0jd", dock_count: 20, city: "09190ajsd0j", installation_date: "8/12/2013")
+    trip = Trip.create!(duration: 123, start_date: DateTime.strptime("8/29/2013 14:13", '%m/%d/%Y %H:%M'), start_station: station_1, end_date: DateTime.strptime("8/29/2013 14:14", '%m/%d/%Y %H:%M'), end_station: station_2, subscription_type: "subscribed", zip_code: 12345, bike_id: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit trips_path(trip)
+
+    click_on 'Edit Trip'
+
+    expect(current_path).to eq(edit_admin_trip_path(trip))
   end
 end

f34d59caa321a61901dbd340fa8ae4092ea1a1e8
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-26 14:23:32 -0600

    add route for admin accessory controller, add index page, style index page, add change status functionality, all tests pass

---
+++ spec/features/root/admin_dashboard_spec.rb
@@ -0,0 +1,62 @@
+require "rails_helper"
+
+describe "an admin" do
+  describe "on the dashboard" do
+    it "clicks on a link for viewing accessories" do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_index_path
+
+      click_on "View Accessories"
+
+      expect(current_path).to eq(admin_bike_shop_path)
+    end
+  end
+
+  describe "accessories dashboard" do
+    it "can see all accessory information" do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+      acc_1 = Accessory.create!(name: "oiad", price: 123, status: 0, image_url: "https://upload.wikimedia.org/wikipedia/commons/1/19/Gatling_gun_1862_Type_II_%281%29.jpg", description: "1209390jioas")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_bike_shop_path
+
+      expect(page).to have_content(acc_1.name)
+
+      expect(page).to have_content("Edit")
+      expect(page).to have_content("Delete")
+      expect(page).to have_content("Deactivate")
+    end
+
+    it "can see activate for retired item" do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+      acc_1 = Accessory.create!(name: "oiad", price: 123, status: 1, image_url: "https://upload.wikimedia.org/wikipedia/commons/1/19/Gatling_gun_1862_Type_II_%281%29.jpg", description: "1209390jioas")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_bike_shop_path
+
+      expect(page).to have_content("Activate")
+    end
+
+    it "can change status if activate is clicked on" do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "ijaidjo", password: "j98jdoas", role: 1)
+      acc_1 = Accessory.create!(name: "oiad", price: 123, status: 1, image_url: "https://upload.wikimedia.org/wikipedia/commons/1/19/Gatling_gun_1862_Type_II_%281%29.jpg", description: "1209390jioas")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_bike_shop_path
+
+      expect(page).to have_content("retired")
+
+      click_on "Activate"
+
+      expect(current_path).to eq(admin_bike_shop_path)
+      expect(page).to have_content("active")
+      expect(page).to_not have_content("retired")
+    end
+  end
+end

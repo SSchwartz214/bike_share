0d1a803059765aa79e5a6cc14eeb59f035fa6c87
Keegan Corrigan <keegan.rw.corrigan@gmail.com>
2018-07-26 15:58:06 -0600

    Revert "Admin bikeship"

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -1,27 +1,6 @@
 class Admin::AccessoriesController < ApplicationController
   before_action :set_accessory, only: [:edit, :update, :destroy]
   before_action :require_admin
-  
-  def new
-    @accessory = Accessory.new
-  end
-
-  def create
-    @accessory = Accessory.new(accessory_params)
-    @accessory.id = assign_id(Accessory)
-    if @accessory.save
-      flash[:notice] = "#{@accessory.name} created successfully!"
-      redirect_to accessory_path(@accessory)
-    else
-      flash[:warning] = "Please choose another name"
-      render :new
-    end
-  end
-
-    private
-    def accessory_params
-      params.require(:accessory).permit(:name, :description, :price, :image, :image_url)
-    end
 
   def index
     @accessories = Accessory.all
@@ -54,6 +33,6 @@ class Admin::AccessoriesController < ApplicationController
   end
 
   def accessory_params
-     params.require(:accessory).permit(:name, :description, :price, :image_url, :status)
+    params.require(:accessory).permit(:status)
   end
 end

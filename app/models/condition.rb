1bdc62d286dded7971f51680c17b21685138d671
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-07-27 16:16:48 -0600

    wip

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -8,7 +8,7 @@ class Condition < ApplicationRecord
   validates_presence_of :mean_wind_speed_mph
   validates_presence_of :precipitation_inches
 
-  def self.trip_during_weather(weather_params)
+  def self.temp_during_trip(weather_params)
     rides = joins("JOIN trips ON conditions.date = trips.start_date")
       .select("conditions.date, count(trips.id) as trip_total")
       .where(max_temperature_f: weather_params)
@@ -20,35 +20,41 @@ class Condition < ApplicationRecord
       [rides.last.trip_total, rides.first.trip_total]
     end
   end
-  #
-  # def self.lowest_rides(weather_params)
-  #   rides = joins("JOIN trips ON conditions.date = trips.start_date")
-  #     .select("conditions.date, count(trips.id) as trip_total")
-  #     .where(max_temperature_f: weather_params)
-  #     .group("conditions.date")
-  #     .order("trip_total desc")
-  #   if rides.first.nil?
-  #     0
-  #   else
-  #     rides.first.trip_total
-  #   end
-  # end
 
-  def self.trip_weather_values(weather_params)
+  def self.trip_temp_values(weather_params)
     weather_params.inject({}) do |hash, (key, value)|
-      hash[key] = Condition.trip_during_weather(value)
+      hash[key] = Condition.temp_during_trip(value)
       hash
     end
   end
 
   def self.heat_map
-    { "30 - 39f" => 80..89,
-      "40 - 49f" => 80..89,
-      "50 - 59f" => 80..89,
-      "60 - 69f" => 80..89,
-      "70 - 79f" => 80..89,
+    { "40 - 49f" => 40..49,
+      "50 - 59f" => 50..59,
+      "60 - 69f" => 60..69,
+      "70 - 79f" => 70..79,
       "80 - 89f" => 80..89,
       "90 - 99f" => 90..99 }
+  end
+
+  def self.prec_map
+    { "0.0 - .49in" => 0.0..0.49,
+      ".50 - .99in" => 0.50..0.99,
+      "1 - 1.49in" => 1..1.49,
+      "1.5 - 1.99in" => 1.5..1.99,
+      "2 - 2.49in" => 2..2.49,
+      "2.5 - 2.99in" => 2.5..2.99,
+      "3 - 3.49in" => 3..3.49,
+      "3.5 - 4in" => 3.5..4 }
+  end
+
+  def self.wind_map
+    { "0 - 3.9mph" => 0..3.9,
+      "4 - 7.9mph" => 4..7.9,
+      "8 - 11.9mph" => 8..11.9,
+      "12 - 15.9mph" => 12..15.9,
+      "16 - 19.9mph" => 16..19.9,
+      "20 - 23.9mph" => 20..23.9 }
   end
 end
 
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -13,7 +13,7 @@ describe Condition, type: :model do
   end
 
   describe "class methods" do
-    it ".highest_rides" do
+    it ".trip_weather_values returns lowest and highest number of rides per temperature range" do
       station_1 = Station.create!(name: "aiojd", dock_count: 8, city: "0912jeioj", installation_date: "8/6/2013")
 
       condition_1 = Condition.create!(date: Date.strptime("8/29/2013", '%m/%d/%Y'), max_temperature_f: 95, mean_temperature_f: 60, min_temperature_f: 123, mean_humidity: 3, mean_visibility_miles: 10, mean_wind_speed_mph: 27, precipitation_inches: 11)
@@ -38,41 +38,9 @@ describe Condition, type: :model do
 
       weather_params = {"80..89f" => 80..89, "90..99f" => 90..99}
 
-      expected = {"80..89f" => 1, "90..99f" => 2}
+      expected = {"80..89f" => [1, 1], "90..99f" => [4, 1]}
 
-      expect(Condition.trip_weather_values(weather_params)).to eq(expected)
-    end
-
-    xit ".lowest_rides" do
-      station_1 = Station.create!(name: "aiojd", dock_count: 8, city: "0912jeioj", installation_date: "8/6/2013")
-
-      condition_1 = Condition.create!(date: Date.strptime("8/29/2013", '%m/%d/%Y'), max_temperature_f: 95, mean_temperature_f: 60, min_temperature_f: 123, mean_humidity: 3, mean_visibility_miles: 10, mean_wind_speed_mph: 27, precipitation_inches: 11)
-
-      condition_2 = Condition.create!(date: Date.strptime("8/1/2013", '%m/%d/%Y'), max_temperature_f: 83, mean_temperature_f: 5115, min_temperature_f: 1235, mean_humidity: 755, mean_visibility_miles: 105, mean_wind_speed_mph: 275, precipitation_inches: 115)
-
-      condition_3 = Condition.create!(date: Date.strptime("7/29/2013", '%m/%d/%Y'), max_temperature_f: 94, mean_temperature_f: 5115, min_temperature_f: 1235, mean_humidity: 755, mean_visibility_miles: 105, mean_wind_speed_mph: 275, precipitation_inches: 115)
-
-      condition_4 = Condition.create!(date: Date.strptime("8/28/2013", '%m/%d/%Y'), max_temperature_f: 95, mean_temperature_f: 60, min_temperature_f: 123, mean_humidity: 3, mean_visibility_miles: 10, mean_wind_speed_mph: 27, precipitation_inches: 11)
-
-      trip_1 = Trip.create!(duration: 100, start_date: DateTime.strptime("7/29/2013", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("8/29/2013", '%m/%d/%Y'), end_station: station_1, subscription_type: "subscribed", zip_code: 12345, bike_id: 1)
-
-      trip_2 = Trip.create!(duration: 200, start_date: DateTime.strptime("8/29/2013", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("7/29/2013 14:17", '%m/%d/%Y %H:%M'), end_station: station_1, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
-
-      trip_3 = Trip.create!(duration: 300, start_date: DateTime.strptime("8/1/2013 14:19", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("8/30/2013 15:19", '%m/%d/%Y %H:%M'), end_station: station_1, subscription_type: "visitor", zip_code: 12446, bike_id: 3)
-
-      trip_4 = Trip.create!(duration: 200, start_date: DateTime.strptime("8/29/2013", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("8/29/2013 14:17", '%m/%d/%Y %H:%M'), end_station: station_1, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
-
-      trip_5 = Trip.create!(duration: 200, start_date: DateTime.strptime("8/29/2013", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("8/29/2013 14:17", '%m/%d/%Y %H:%M'), end_station: station_1, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
-
-      trip_6 = Trip.create!(duration: 200, start_date: DateTime.strptime("8/29/2013", '%m/%d/%Y'), start_station: station_1, end_date: DateTime.strptime("8/29/2013 14:17", '%m/%d/%Y %H:%M'), end_station: station_1, subscription_type: "visitor", zip_code: 12444, bike_id: 2)
-
-      binding.pry
-
-      weather_params = {"80..89f" => 80..89, "90..99f" => 90..99}
-
-      expected = {"80..89f" => 1, "90..99f" => 2}
-
-      expect(Condition.trip_weather_values(weather_params, trip_weather_values)).to eq(expected)
+      expect(Condition.trip_temp_values(weather_params)).to eq(expected)
     end
   end
 end

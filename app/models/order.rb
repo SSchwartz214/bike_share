459bc217cea41dcea5d559a819efd4b0385acacb
Connor <connorosburn@gmail.com>
2018-07-27 17:10:40 -0600

    About to get real

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ApplicationRecord
-  validates_presence_of :status
+  validates_presence_of :status, default: "ordered"
 
   belongs_to :user
   has_many :order_accessories
   has_many :accessories, through: :order_accessories
 
   def total
-    
+    order_accessories.inject(0) {|sum, order_accessory| sum + order_accessory.subtotal}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726212719) do
+ActiveRecord::Schema.define(version: 20180727221915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20180726212719) do
     t.string "username"
     t.string "password_digest"
     t.integer "role", default: 0
+    t.string "address"
   end
 
   add_foreign_key "order_accessories", "accessories"

169120eb99d90ed33082302542eb2b07638e66c7
Seth Schwartz <sschwartz214@gmail.com>
2018-07-27 18:24:14 -0600

    creates index page for admin order dashboard.  All tests passing

--- spec/features/root/admin_dashboard_spec.rb
+++ spec/features/root/admin_dashboard_spec.rb
@@ -75,5 +75,40 @@ describe "an admin" do
       expect(Accessory.count).to eq(0)
       expect(page).to_not have_content(acc_1.description)
     end
+
+#     As an admin user,
+# When I visit the admin dashboard,
+# I see a list of all orders,
+# I see the total number of orders for each status ("Ordered", "Paid", "Cancelled", "Completed"),
+# I see a link for each individual order,
+# I can filter orders to display by each status type ("Ordered", "Paid", "Cancelled", "Completed"),
+# I have links to transition between statuses
+#
+# I can click on "cancel" on individual orders which are "paid" or "ordered"
+# I can click on "mark as paid" on orders that are "ordered"
+# I can click on "mark as completed" on orders that are "paid"
+
+    it 'displays a list of all orders with attributes and links' do
+      admin = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "admin", password: "j98jdoas", role: 1)
+      user = User.create!(first_name: "oijasdioj", last_name: "ijd098jas", username: "user", password: "j98jdoas", role: 0)
+      order = user.orders.create
+      order_2 = user.orders.create
+      accessory = Accessory.create(name: "oiad", price: 123, status: 0, image_url: "https://upload.wikimedia.org/wikipedia/commons/1/19/Gatling_gun_1862_Type_II_%281%29.jpg", description: "1209390jioas")
+      order_accessory = order.order_accessories.create(quantity: 5, accessory_id: accessory.id, order_id: order.id)
+      order_accessory = order.order_accessories.create(quantity: 5, accessory_id: accessory.id, order_id: order_2.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_index_path
+
+      expect(page).to have_content(order.id)
+      expect(page).to have_content(order.created_at)
+      expect(page).to have_content(order.user.full_name)
+      expect(page).to have_content(order.status)
+      expect(page).to have_content(order_2.id)
+      expect(page).to have_content(order_2.created_at)
+      expect(page).to have_content(order_2.user.full_name)
+      expect(page).to have_content(order_2.status)
+    end
   end
 end
